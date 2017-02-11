b732d71b03d87385b9c9a1a79d39e231729d8423
Manjunath Kempaiah <manjunath.kmph@gmail.com>
2017-02-11 23:05:29 +0530

    Initial Commit

---
+++ src/com/manju/examples/java8features/PredicatesDemo.java
@@ -0,0 +1,28 @@
+package com.manju.examples.java8features;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class PredicatesDemo {
+
+	public static void main(String[] args) {
+		List<Integer> numbers = Arrays.asList(0,1,2,3,4,5,6,7,8,9,10);
+		List<String> stringNumbers = Arrays.asList(new String("One"), new String("Two"), new String("Three"));
+		PredicateDemo pDemo = new PredicateDemo();
+		System.out.println("Print all the numbers : " + pDemo.testPredicate01(x -> true, numbers));
+		System.out.println("Print all the even numbers : " + pDemo.testPredicate01(x -> (x & 1) == 0, numbers));
+		System.out.println("Print all the odd numbers : " + pDemo.testPredicate01(x -> (x & 1) == 1, numbers));
+		
+		System.out.println("Print all the numbers : " + pDemo.testPredicate01(x -> true, stringNumbers));
+		System.out.println("Print all the even numbers : " + pDemo.testPredicate01(x -> x.contains("e"), stringNumbers));
+		System.out.println("Print all the odd numbers : " + pDemo.testPredicate01(x -> x.contains("T"), stringNumbers));
+		
+		System.out.println("Print all the numbers : -->Negate: " + pDemo.testPredicateNegate(x -> true, numbers));
+		System.out.println("Print all the even numbers : -->Negate: " + pDemo.testPredicateNegate(x -> (x & 1) == 0, numbers));
+		System.out.println("Print all the odd numbers : -->Negate : " + pDemo.testPredicateNegate(x -> (x & 1) == 1, numbers));
+		
+		System.out.println("Print even or odd numbers : " + pDemo.testPredicateOr(x -> (x & 1) == 0, x -> (x & 1) == 1, numbers));
+		System.out.println("Print even and odd numbers : " + pDemo.testPredicateAnd(x -> (x & 1) == 0, x -> (x & 1) == 1, numbers));
+	}
+
+}
